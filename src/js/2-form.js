7917350b03955d647858dfdfa3f6c9ccda731f85
Chyshok <taraschisok@gmail.com>
2025-12-02 17:53:42 +0100

    seems, i did properly

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,57 +1,29 @@
-// Додай у HTML розмітку форми. У JS напиши скрипт, який буде зберігати значення полів у локальне сховище, коли користувач щось друкує.
-
-// Виконуй це завдання у файлах 2-form.html і 2-form.js. Розбий його на декілька підзавдань:
-
-// Оголоси поза будь-якими функціями об’єкт formData з полями email та message, які спочатку мають порожні рядки як значення: { email: "", message: "" }.
-// Використовуй метод делегування для відстеження змін у формі через подію input. Зберігай актуальні дані з полів email та message у formData та записуй цей об’єкт у локальне сховище. Використовуй ключ "feedback-form-state" для зберігання даних у сховищі.
-// При завантаженні сторінки перевір, чи є дані у локальному сховищі. Якщо так, використовуй їх для заповнення форми та об'єкта formData. Якщо ні, залиш поля форми порожніми.
-// Перед відправленням форми переконайся, що обидва поля форми заповнені. Якщо будь-яке з полів (властивостей об’єкта formData) порожнє, показуй сповіщення з текстом «Fill please all fields». Якщо всі поля заповнені, виведи у консоль об’єкт formData з актуальними значеннями, очисти локальне сховище, об’єкт formData і поля форми.
-
-// На живій сторінці відображається форма з двома елементами форми і кнопкою типу submit
-// Форма стилізована згідно з макетом
-// На формі прослуховуються події input і submit
-// При введенні даних у будь-який елемент форми вони записуються у локальне сховище під ключем "feedback-form-state", збережені дані не містять пробіли по краях
-// Введення даних в одне поле форми не видаляє дані в сховищі для іншого
-// При оновленні сторінки дані з локального сховища підставляються в елементи форми, у полях форми відсутні undefined
-// При сабміті форми є перевірка, щоб обидва елементи форми були заповнені
-// Під час сабміту форми, якщо обидва елементи форми заповнені, виводиться у консоль об'єкт з полями email, message та їхніми поточними значеннями, а також очищаються сховище і поля форми
-// Якщо після сабміту форми ввести в будь-який елемент форми дані, то в локальному сховищі не з’являються дані від попереднього сабміта
 
 const formData = {
     email: "",
     message: "",
 };
 
-// const feedbackFormState = localStorage.getItem('feedback-form-state').parse();
-
-// if(feedbackFormState.email && feedbackFormState.message !== "" || null){
-// form.elements.email.value = localStorage.feedback-form-state.email.parse().email;
-// form.elements.message.value = localStorage.feedback-form-state.message.parse().message;}
-
 const form = document.querySelector("form.feedback-form");
-form.addEventListener("input", (event) => {
-    const item = localStorage.getItem('feedback-form-state'); try {
-    if(JSON.parse(localStorage.getItem('feedback-form-state')).email && localStorage.getItem('feedback-form-state').message !== ""){
-
-    }} catch {console.log("error with JSON.parse")};
+form.addEventListener("input", () => {
+    form.elements.email.value.trim() = formData.email;
+    form.elements.message.value.trim() = formData.message;
+    localStorage.setItem('feedback-form-state', formData.stringify())
 });
 
-form.addEventListener("submit", (event) => {
-    if(form.email === "" && form.message === ""){
+form.addEventListener("submit", () => {
+    if(form.elements.email.value.trim() === "" && form.elements.message.value.trim() === ""){
 return alert("Fill please all fields");
     } else {
-    formData.email = form.elements.email.value.trim();
-    formData.message = form.elements.message.value.trim();
-    console.log(formData);
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-    console.log(localStorage);
+        if(form.elements.email.value.trim() !== "" ){
+            formData.email = form.elements.email.value.trim();
+        } 
+        if(form.elements.message.value.trim() !== ""){
+            formData.message = form.elements.message.value.trim();
+        }
+    localStorage.setItem("feedback-form-state", formData.stringify());
     //очистити лок схов, об'єкт, поля форми;
-    form.elements.email.value = "";
-    form.elements.message.value = "";
-    document.querySelector(".feedback-form").reset;
-    localStorage.clear();
-    console.log(localStorage);
+    document.querySelector(".feedback-form").reset();
+    localStorage.removeItem('feedback-from-state');
     }
 });
-
-// перевірити при завантаженні сторінки чи є данні в локальному сховищі і підставити їх до полів.
