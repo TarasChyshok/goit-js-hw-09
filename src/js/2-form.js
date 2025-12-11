c1524bfd2774421752cf93700ffcfee973aedb5a
Chyshok <taraschisok@gmail.com>
2025-12-12 00:01:29 +0100

    edited by review: deleted elements in form submit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,7 +11,6 @@ const parsedItemFromLocalStorage = JSON.parse(localStorage.getItem("feedback-for
 if(getItemFromLocalStorage !== null){
     formData.email = parsedItemFromLocalStorage.email;
     formData.message = parsedItemFromLocalStorage.message;
-    console.log(formData);
     form.elements.email.value = formData.email;
     form.elements.message.value = formData.message;
 }
@@ -29,20 +28,10 @@ form.addEventListener("submit", (event) => {
     if(form.elements.email.value.trim() === "" || form.elements.message.value.trim() === ""){
 return alert("Fill please all fields");
     } else {
-        if(form.elements.email.value.trim() !== "" ){
-            formData.email = form.elements.email.value.trim();
-        } 
-        if(form.elements.message.value.trim() !== ""){
-            formData.message = form.elements.message.value.trim();
-        }
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-    console.log(localStorage);
-    //очистити лок схов, об'єкт, поля форми;
     document.querySelector(".feedback-form").reset();
     localStorage.removeItem('feedback-form-state');
     console.log(formData);
-    console.log(localStorage);
-    delete formData.email;
-    delete formData.message;
+    formData.email = "";
+    formData.message = "";
     }
 });
